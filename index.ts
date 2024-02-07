dc0955ae9c07672941025c0001f4f8316aae1e71
Alfian Prisma Yopiangga <51021806+yopiangga@users.noreply.github.com>
2024-02-07 19:35:35 +0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -30,6 +30,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Replace '*' with your allowed origins
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
+
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.get("/", (req, res) => {
